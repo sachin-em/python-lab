cbffdf97b2f2a37d2ab6c6ae740ecce1f4d2aef0
Sachin M <45335064+sachin-em@users.noreply.github.com>
2021-11-15 11:40:47 +0530

    Add files via upload

---
+++ Exercise 4 (Over)/over.py
@@ -0,0 +1,16 @@
+# read list of integers and replace integers greater than 100 with over
+
+ints = input("Enter a list of integers seperated by space : ")
+intList = ints.split(" ")
+
+
+index = 0
+for integer in intList:
+     
+     if int(integer) > 100:
+          intList[index] = "Over"
+          
+     index += 1
+
+
+print(intList)
