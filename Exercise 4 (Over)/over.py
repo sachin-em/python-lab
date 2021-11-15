2a780ef6b47b37a1e82ca116b09b1559797db0bb
Sachin M <45335064+sachin-em@users.noreply.github.com>
2021-11-15 11:41:33 +0530

    Update over.py

--- Exercise 4 (Over)/over.py
+++ Exercise 4 (Over)/over.py
@@ -3,14 +3,11 @@
 ints = input("Enter a list of integers seperated by space : ")
 intList = ints.split(" ")
 
-
 index = 0
-for integer in intList:
-     
+for integer in intList:     
      if int(integer) > 100:
           intList[index] = "Over"
           
      index += 1
-
 
 print(intList)
