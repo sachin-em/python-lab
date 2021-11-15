19acda5585ba36aabc66b486622f9e35fcc564bf
Sachin M <45335064+sachin-em@users.noreply.github.com>
2021-11-15 11:51:49 +0530

    Update circle.py

--- Exercise 1(Area and Perimeter of  a Circle)/circle.py
+++ Exercise 1(Area and Perimeter of  a Circle)/circle.py
@@ -6,4 +6,4 @@ radius = int(input(" Enter radius of the circle : "))
 area = pi * radius * radius
 perimeter = 2 * pi * radius
 
-print(" Area = ",area,"\n Perimeter = ",perimeter)
+print(" Area = ",area,"\n Circumference = ",perimeter)
